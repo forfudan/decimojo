76ced5b7a8c30b1c21d4974cc07eb2a32057081c
ZHU Yuhao <dr.yuhao.zhu@outlook.com>
2026-02-19 23:56:50 +0100

    add tests to toml

--- tests/test_binary.sh
+++ tests/test_binary.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e  # Exit immediately if any command fails
 
-for dir in biguint bigint bigdecimal bigbinaryint decimal128; do
+for dir in bigbinaryint; do
     for f in tests/$dir/*.mojo; do
         pixi run mojo run -I src -D ASSERT=all "$f"
     done
